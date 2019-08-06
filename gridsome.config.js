df5e54fab5027fc9abede478a5177143fbf04a80
Tyler VanBlargan <12vanblart@gmail.com>
2019-08-06 07:22:37 -0400

    🎉 Init

---
+++ gridsome.config.js
@@ -0,0 +1,24 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-airtable',
+      options: {
+        // Add these to a .env file
+        // Details on finding these values can be found at:
+        // https://gridsome.org/plugins/@gridsome/source-airtable
+        apiKey: process.env.AIRTABLE_KEY, //required
+        baseId: process.env.AIRTABLE_BASE, //required
+        tableName: 'Events', //required
+        typeName: 'Event', //required
+        route: '/events/:name' //optional
+      }
+    }
+  ]
+}
